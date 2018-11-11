f1253b4cd20ef0b46848592911aa7ee379523180
Anirudh Palle <anirudh.palle@nokia.com>
2018-11-11 14:23:45 +0530

    Reverse the list

--- inc/linkedList.h
+++ inc/linkedList.h
@@ -6,8 +6,10 @@ typedef struct node {
    struct node *next;
 }intNode;
 
-int append(intNode *head, int value);
+int append(intNode **head, int value);
 
 intNode* delete(intNode *head, int value);
+
+intNode* reverseList(intNode* head);
 
 int printList(intNode *head);
